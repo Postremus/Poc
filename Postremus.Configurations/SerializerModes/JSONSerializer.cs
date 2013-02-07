20cb0cfe4bbf0750d2ca73f1fe45c9a594e20636
Postremus <panzer1996@googlemail.com>
2013-02-07 09:27:28 +0100

    Den JSONSerializer implementiert

---
+++ Postremus.Configurations/SerializerModes/JSONSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace Poc.SerializerModes
+{
+    class JSONSerializer : ISerializerMode
+    {
+        DataContractJsonSerializer _seri;
+
+        public JSONSerializer()
+        {
+        }
+
+        public void Serialize<T>(System.IO.Stream serialisationStream, T value)
+        {
+            _seri = new DataContractJsonSerializer(typeof(T));
+            _seri.WriteObject(serialisationStream, value);
+        }
+
+        public T Deserialize<T>(System.IO.Stream serialisationStream)
+        {
+            _seri = new DataContractJsonSerializer(typeof(T));
+            return (T)_seri.ReadObject(serialisationStream);
+        }
+    }
+}
